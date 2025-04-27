7d3da9fef537bb2915bc772f8ba74554f673430c
Monisha-123-jpg <ishakumar3096@gmail.com>
2025-04-27 23:35:51 +0530

    Add files via upload
    Added day 1 assignments

---
+++ day1/FirstClass.java
@@ -0,0 +1,11 @@
+package week1.day1;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi all,welcome to testleaf");
+		// TODO Auto-generated method stub
+
+	}
+
+}
